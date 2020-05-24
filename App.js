6ffe2bc2ac6ba87237506936a032cbbf4a1b126d
Jessica <Jessica@users-iMac.local>
2020-05-23 21:13:23 -0400

    Step 1. add Omega-Web-App as a submodule

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { add } from '../common/index.js';
+import { add } from '../Omega-Web-App/packages/common/index.js';
 
 export default function App() {
 	return (
